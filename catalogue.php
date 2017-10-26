04c94db40e534a1d790b6c5412046b2217d6798d
Dan Marsden <dan@danmarsden.com>
2017-10-26 15:11:27 +1300

    Remove reference to deprecatd admin/tool/topics (hla 2.9)

--- catalogue.php
+++ catalogue.php
@@ -24,7 +24,6 @@
  */
 
 require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
-require_once($CFG->dirroot . '/admin/tool/topics/lib.php');
 require_once(dirname(__FILE__) . '/locallib.php');
 require_once(dirname(__FILE__) . '/catalogue_form.php');
 
